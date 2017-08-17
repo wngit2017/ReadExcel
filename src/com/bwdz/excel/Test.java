eb215fc591d2745a93d78cd8819b6c5d6d83d33d
Mr.King <hellowang_ok@163.com>
2017-08-17 17:26:44 +0800

    init commit

---
+++ src/com/bwdz/excel/Test.java
@@ -0,0 +1,12 @@
+package com.bwdz.excel;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asdfasdfasdfasdf");
+		System.out.println("1212121212121212");
+		System.out.println("-==============-");
+	}
+
+}
---
+++ src/com/bwdz/myjf/TestMyjf.java
@@ -0,0 +1,25 @@
+package com.bwdz.myjf;
+
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.AlipayOpenPublicMenuCreateRequest;
+
+public class TestMyjf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AlipayClient alipayClient = new DefaultAlipayClient("https://openapi.alipay.com/gateway.do", 
+															"app_id",
+															"yourprivate_key",
+															"json",
+															"GBK",
+															"alipay_public_key");
+		
+		
+//		AlipayOpenPublicMenuCreateRequest
+		
+		
+		
+	}
+
+}
